56d5f377ee74057df97a63a8c918b671655fbbd0
TheOnlyMace <0815cracky@gmail.com>
2026-02-26 21:07:43 +0100

    Remove provisioning module files including hooks, main module, templates, logo, and test files.

---
+++ .stubs/whmcs.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * WHMCS function stubs for static analysis / IDE support.
+ * These functions are provided by the WHMCS runtime environment.
+ *
+ * @see https://developers.whmcs.com/
+ */
+
+/**
+ * @param string $module
+ * @param string $action
+ * @param mixed  $request
+ * @param mixed  $response
+ * @param mixed  $processedData
+ * @param mixed  $replaceVars
+ * @return void
+ */
+function logModuleCall($module, $action, $request, $response, $processedData = '', $replaceVars = []) {}
+
+/**
+ * @param string $data
+ * @return string
+ */
+function decrypt($data) { return ''; }
